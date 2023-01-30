16bbfd7233881772de230a0ed3d513ec8ca77913
abdulec90 <abdulec90@gmail.com>
2023-01-30 15:10:55 +0530

    save locals

--- src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
@@ -1,13 +1,11 @@
 package guru.springframework.msscbrewery.web.model;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Null;
-import javax.validation.constraints.Positive;
 import java.util.UUID;
 
 /**
